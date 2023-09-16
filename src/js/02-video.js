5905d0868bc8906805287a60c70fcad3a6054715
ViktoriaSharova <131592560+ViktoriaSharova@users.noreply.github.com>
2023-09-16 17:53:08 +0200

    Initial commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,30 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const TIME_KEY = 'videoplayer-current-time';
-
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-const getCurrentTime = function (currentTime) {
-  const seconds = currentTime.seconds;
-  localStorage.setItem(TIME_KEY, JSON.stringify(seconds));
+const onPlay = function(data) {
+    localStorage.setItem(`videoplayer-current-time`, JSON.stringify(data.seconds));
 };
 
-player.on('timeupdate', throttle(getCurrentTime, 1000));
+const throttleOnPlay = throttle(onPlay, 1000);
 
-player.setCurrentTime(JSON.parse(localStorage.getItem(TIME_KEY)) || 0);
+player.on('timeupdate', throttleOnPlay);
+
+const savedTime = localStorage.getItem("videoplayer-current-time");
+const parsedSavedTime = JSON.parse(savedTime);
+
+player.setCurrentTime(parsedSavedTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
