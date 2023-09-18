e3e77306d5b9e9b27923aeddc5080d85588e44bb
ViktoriaSharova <131592560+ViktoriaSharova@users.noreply.github.com>
2023-09-18 19:00:20 +0200

    Initial commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,10 +11,17 @@ formEl.addEventListener('input', throttle(formDataInput, 500));
 formEl.addEventListener('submit', formClearAfterSubmit);
 
 // Функція для запису даних в localStorage з formData
+// function formDataInput(event) {
+//     formData[event.target.name] = event.target.value;
+//     localStorage.setItem(FORM_LOCAL_SROREGE_KEY, JSON.stringify(formData));
+// };
+
 function formDataInput(event) {
-    formData[event.target.name] = event.target.value;
-    localStorage.setItem(FORM_LOCAL_SROREGE_KEY, JSON.stringify(formData));
-};
+  const FORM_LOCAL_STORAGE_KEY = "feedback-form-state";
+  let formData = JSON.parse(localStorage.getItem(FORM_LOCAL_STORAGE_KEY)) || {};
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(FORM_LOCAL_STORAGE_KEY, JSON.stringify(formData));
+}
 
 // Перевірка стану сховища, і якщо там є збережені дані, заповнюються ними поля форми. В іншому випадку поля - порожні.
 formFillCheck();
@@ -38,5 +45,5 @@ function formClearAfterSubmit(event) {
     formData.email = formEl.elements.email.value;
     formData.message = formEl.elements.message.value;
     formEl.reset();
-    localStorage.removeItem(FORM_LOCAL_SROREGE_KEY);
+    localStorage.removeItem("FORM_LOCAL_SROREGE_KEY");
 };
